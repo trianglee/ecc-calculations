c2bf49b8391119a990d5936d4e0e698419fe4fbf
Nimrod Zimerman <zimerman@fastmail.fm>
2021-08-20 01:09:55 +0300

    Derive valid public keys from signature

--- ecc-calculations.py
+++ ecc-calculations.py
@@ -5,6 +5,8 @@ import Crypto.Signature.DSS
 import tinyec.registry
 import tinyec.ec
 
+import nummaster.basic
+
 import sys
 
 # References -
@@ -14,8 +16,9 @@ import sys
 # https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages
 # http://www.secg.org/sec1-v2.pdf (4.1.4 Verifying Operation)
 # http://www.secg.org/sec2-v2.pdf (2.4.2 Recommended Parameters secp256r1)
+# https://cryptobook.nakov.com/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc
 
-key = Crypto.PublicKey.ECC.generate(curve='P-256')
+key = Crypto.PublicKey.ECC.generate(curve="P-256")
 print (f"key = {key}")
 
 message = b'Message'
@@ -68,13 +71,14 @@ G = tinyec.ec.Point(curve, G_x, G_y)
 print (f"G = {G}")
 
 # "ECDSA Verify Signature" from https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages -
+# (and also "4.1.4 Verifying Operation" from http://www.secg.org/sec1-v2.pdf, with slightly different names)
 
 s1 = pow(s, -1, n)
 print (f"s1 = {s1}")
 
 # R' = (h * s1) * G + (r * s1) * pubKey
 
-h = int(digest.hexdigest(), 16)
+h = int(digest.hexdigest(), 16)  # Called "e" in 4.1.4.
 print (f"h = {h}")
 public_key = tinyec.ec.Point(curve, int(key.public_key().pointQ.x), int(key.public_key().pointQ.y))
 print (f"public_key = {public_key}")
@@ -92,3 +96,78 @@ if r_tag == r:
 else:
     print ("----> Signature DOESN'T match!")
     sys.exit(1)
+
+
+print ("=== Public Key from Signature Using ECC Calculations ===")
+
+# p, a, b taken from "2.4.2 Recommended Parameters secp256r1" in http://www.secg.org/sec2-v2.pdf -
+p = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
+a = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC
+b = 0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B
+print (f"p = {p}")
+print (f"a = {a}")
+print (f"b = {b}")
+
+# Taken from https://cryptobook.nakov.com/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc, unused.
+# def uncompress_point(compressed_point, p, a, b):
+#     x, is_odd = compressed_point
+#     y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
+#     if bool(is_odd) == bool(y & 1):
+#         return (x, y)
+#     return (x, p - y)
+
+# Derive the y-component of a compressed point, given the x-component only, assuming the y-component is odd.
+def uncompress_point_odd(x, p, a, b):
+    y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
+    return y
+
+
+# "4.1.6 Public Key Recovery Operation" from http://www.secg.org/sec1-v2.pdf -
+
+candidate_public_key_points = []
+
+for j in [0, 1]:  # "h" of the curve is 1.
+
+    x = r + j*n
+    print (f"x = {x}")
+    try:
+        y = uncompress_point_odd(x, p, a, b)
+        print (f"y = {y}")
+    except Exception:
+        print ("Decompressing point failed, trying next.")
+        continue
+
+    R = tinyec.ec.Point(curve, x, y)
+    print (f"R = {R}")
+
+    n_dot_R = n*R
+    print (f"n*R = {n_dot_R}")
+    if n_dot_R != tinyec.ec.Inf(curve):
+        print ("n*R isn't Inf, trying next.")
+        continue
+
+    e = int(digest.hexdigest(), 16)
+    for k in [1,-1]:  # Try both with a positive and a negative R.
+        Q = pow(r, -1, n) * (s*(k*R) - e*G)
+        print(f"Q = {Q}")
+
+        candidate_public_key_points.append(Q)
+
+
+valid_public_keys = []
+for candidate_public_key_point in candidate_public_key_points:
+    Crypto_candidate_public_key_point = Crypto.PublicKey.ECC.EccPoint(candidate_public_key_point.x, candidate_public_key_point.y, curve="P-256")
+    Crypto_candidate_public_key = Crypto.PublicKey.ECC.EccKey(point=Crypto_candidate_public_key_point, curve="P-256")
+
+    verifier = Crypto.Signature.DSS.new(Crypto_candidate_public_key, 'fips-186-3')
+    try:
+        verifier.verify(digest, signature)
+        # Signature matches, store the public key.
+        valid_public_keys.append (Crypto_candidate_public_key)
+    except ValueError:
+        # Signature doesn't match, continue.
+        continue
+
+print ("----> Valid public keys that can verify the signature -")
+for valid_public_key in valid_public_keys:
+    print (valid_public_key)
