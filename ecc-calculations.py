ccd998116880f06e7e2f94d7b7a5b481e0cb6d4e
Nimrod Zimerman <zimerman@fastmail.fm>
2021-08-20 01:56:39 +0300

    Re-factored into functions and a bit more

--- ecc-calculations.py
+++ ecc-calculations.py
@@ -18,156 +18,218 @@ import sys
 # http://www.secg.org/sec2-v2.pdf (2.4.2 Recommended Parameters secp256r1)
 # https://cryptobook.nakov.com/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc
 
-key = Crypto.PublicKey.ECC.generate(curve="P-256")
-print (f"key = {key}")
 
-message = b'Message'
-print (f"message = {message}")
+# NIST P-256 / secp256r1 curve parameters -
+# Taken from "2.4.2 Recommended Parameters secp256r1" in http://www.secg.org/sec2-v2.pdf
+# (also available in tinyec.registry.EC_CURVE_REGISTRY["secp256r1"])
 
-digest = Crypto.Hash.SHA256.new(message)
-print (f"digest = {digest.hexdigest()}")
+CURVE_NAME = "secp256r1"  # Also known as "NIST P-256"
 
-signer = Crypto.Signature.DSS.new(key, 'fips-186-3')  # fips-186-3 - concatenated big-endian r,s
-signature = signer.sign(digest)
-print (f"signature = {signature.hex()}")
+curve = tinyec.registry.get_curve(CURVE_NAME)
+curve_params = tinyec.registry.EC_CURVE_REGISTRY[CURVE_NAME]
 
-print ("=== Signature Verification Using PyCryptodome ===")
+p = curve_params["p"]
+a = curve_params["a"]
+b = curve_params["b"]
+G = tinyec.ec.Point(curve, curve_params["g"][0], curve_params["g"][1])
+n = curve_params["n"]
 
-verifier = Crypto.Signature.DSS.new(key.public_key(), 'fips-186-3')
-try:
-    verifier.verify(digest, signature)
-    print ("----> Signature matches.")
-except ValueError:
-    print ("----> Signature DOESN'T match!")
-    sys.exit(1)
+###
 
+CRYPTO_DOME_RANDOM_SIGNATURE_MODE = "fips-186-3"  # Random signature generation
 
-print ("=== Signature Verification Using ECC Calculations ===")
-
-curve = tinyec.registry.get_curve("secp256r1")
-
-r_bin = signature[0:32]
-s_bin = signature[32:64]
-print(f"r_bin = {r_bin.hex()}")
-print(f"s_bin = {s_bin.hex()}")
-
-if len(r_bin) + len(s_bin) != len(signature):
-    print ("Signature is not of the expected size!")
-    sys.exit(1)
-
-r = int.from_bytes(r_bin, "big")
-s = int.from_bytes(s_bin, "big")
-print (f"r = {r}")
-print (f"s = {s}")
-
-# n and G taken from "2.4.2 Recommended Parameters secp256r1" in http://www.secg.org/sec2-v2.pdf -
-
-n = 0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551
-print (f"n = {n}")
-
-G_x = 0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296
-G_y = 0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5
-G = tinyec.ec.Point(curve, G_x, G_y)
-print (f"G = {G}")
-
-# "ECDSA Verify Signature" from https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages -
-# (and also "4.1.4 Verifying Operation" from http://www.secg.org/sec1-v2.pdf, with slightly different names)
-
-s1 = pow(s, -1, n)
-print (f"s1 = {s1}")
-
-# R' = (h * s1) * G + (r * s1) * pubKey
-
-h = int(digest.hexdigest(), 16)  # Called "e" in 4.1.4.
-print (f"h = {h}")
-public_key = tinyec.ec.Point(curve, int(key.public_key().pointQ.x), int(key.public_key().pointQ.y))
-print (f"public_key = {public_key}")
-
-R_tag_a = (h * s1) * G
-R_tag_b = (r * s1) * public_key
-R_tag = R_tag_a + R_tag_b
-print (f"R_tag = {R_tag}")
-
-r_tag = R_tag.x
-print (f"r_tag = {r_tag}")
-
-if r_tag == r:
-    print ("----> Signature matches.")
-else:
-    print ("----> Signature DOESN'T match!")
-    sys.exit(1)
+###
 
 
-print ("=== Public Key from Signature Using ECC Calculations ===")
-
-# p, a, b taken from "2.4.2 Recommended Parameters secp256r1" in http://www.secg.org/sec2-v2.pdf -
-p = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
-a = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC
-b = 0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B
-print (f"p = {p}")
-print (f"a = {a}")
-print (f"b = {b}")
-
-# Taken from https://cryptobook.nakov.com/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc, unused.
-# def uncompress_point(compressed_point, p, a, b):
-#     x, is_odd = compressed_point
-#     y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
-#     if bool(is_odd) == bool(y & 1):
-#         return (x, y)
-#     return (x, p - y)
-
-# Derive the y-component of a compressed point, given the x-component only, assuming the y-component is odd.
-def uncompress_point_odd(x, p, a, b):
-    y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
-    return y
+def generate_keypair():
+    key = Crypto.PublicKey.ECC.generate(curve=CURVE_NAME)
+    return key
 
 
-# "4.1.6 Public Key Recovery Operation" from http://www.secg.org/sec1-v2.pdf -
+def generate_signature(keypair, message):
+    print (f"keypair = {keypair}")
+    print (f"message = {message}")
 
-candidate_public_key_points = []
+    digest = Crypto.Hash.SHA256.new(message)
+    print (f"digest = {digest.hexdigest()}")
 
-for j in [0, 1]:  # "h" of the curve is 1.
+    signer = Crypto.Signature.DSS.new(keypair, CRYPTO_DOME_RANDOM_SIGNATURE_MODE)
+    signature = signer.sign(digest)
+    print (f"signature = {signature.hex()}")
 
-    x = r + j*n
-    print (f"x = {x}")
-    try:
-        y = uncompress_point_odd(x, p, a, b)
-        print (f"y = {y}")
-    except Exception:
-        print ("Decompressing point failed, trying next.")
-        continue
-
-    R = tinyec.ec.Point(curve, x, y)
-    print (f"R = {R}")
-
-    n_dot_R = n*R
-    print (f"n*R = {n_dot_R}")
-    if n_dot_R != tinyec.ec.Inf(curve):
-        print ("n*R isn't Inf, trying next.")
-        continue
-
-    e = int(digest.hexdigest(), 16)
-    for k in [1,-1]:  # Try both with a positive and a negative R.
-        Q = pow(r, -1, n) * (s*(k*R) - e*G)
-        print(f"Q = {Q}")
-
-        candidate_public_key_points.append(Q)
+    return signature
 
 
-valid_public_keys = []
-for candidate_public_key_point in candidate_public_key_points:
-    Crypto_candidate_public_key_point = Crypto.PublicKey.ECC.EccPoint(candidate_public_key_point.x, candidate_public_key_point.y, curve="P-256")
-    Crypto_candidate_public_key = Crypto.PublicKey.ECC.EccKey(point=Crypto_candidate_public_key_point, curve="P-256")
+def verify_signature_cryptodome(public_key, message, signature):
 
-    verifier = Crypto.Signature.DSS.new(Crypto_candidate_public_key, 'fips-186-3')
+    digest = Crypto.Hash.SHA256.new(message)
+    print (f"digest = {digest.hexdigest()}")
+
+    verifier = Crypto.Signature.DSS.new(public_key, CRYPTO_DOME_RANDOM_SIGNATURE_MODE)
     try:
         verifier.verify(digest, signature)
-        # Signature matches, store the public key.
-        valid_public_keys.append (Crypto_candidate_public_key)
+        return True
     except ValueError:
-        # Signature doesn't match, continue.
-        continue
+        return False
 
-print ("----> Valid public keys that can verify the signature -")
-for valid_public_key in valid_public_keys:
-    print (valid_public_key)
+
+def extract_r_s_from_signature(signature):
+
+    # Each signature component size is exactly 32 bytes (256 bits).
+    r_bin = signature[0:32]
+    s_bin = signature[32:64]
+    print(f"r_bin = {r_bin.hex()}")
+    print(f"s_bin = {s_bin.hex()}")
+
+    if (len(signature) != 64) or (len(r_bin) + len(s_bin) != len(signature)):
+        print ("Signature is not of the expected size!")
+        sys.exit(1)
+
+    r = int.from_bytes(r_bin, "big")
+    s = int.from_bytes(s_bin, "big")
+    print (f"r = {r}")
+    print (f"s = {s}")
+
+    return (r,s)
+
+
+def verify_signature_ecc_calcs(public_key, message, signature):
+
+    (r,s) = extract_r_s_from_signature(signature)
+
+    # "ECDSA Verify Signature" from https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-messages -
+    # (and also "4.1.4 Verifying Operation" from http://www.secg.org/sec1-v2.pdf, with slightly different names)
+
+    s1 = pow(s, -1, n)
+    print (f"s1 = {s1}")
+
+    # R' = (h * s1) * G + (r * s1) * pubKey
+
+    digest = Crypto.Hash.SHA256.new(message)
+    print (f"digest = {digest.hexdigest()}")
+
+    h = int(digest.hexdigest(), 16)  # Called "e" in 4.1.4.
+    print (f"h = {h}")
+    public_key = tinyec.ec.Point(curve, int(public_key.pointQ.x), int(public_key.pointQ.y))
+    print (f"public_key = {public_key}")
+
+    R_tag_a = (h * s1) * G
+    R_tag_b = (r * s1) * public_key
+    R_tag = R_tag_a + R_tag_b
+    print (f"R_tag = {R_tag}")
+
+    r_tag = R_tag.x
+    print (f"r_tag = {r_tag}")
+
+    if r_tag == r:
+        return True
+    else:
+        return False
+
+
+def public_keys_from_signature(message, signature):
+
+    # Taken from https://cryptobook.nakov.com/asymmetric-key-ciphers/elliptic-curve-cryptography-ecc, unused.
+    # def uncompress_point(compressed_point, p, a, b):
+    #     x, is_odd = compressed_point
+    #     y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
+    #     if bool(is_odd) == bool(y & 1):
+    #         return (x, y)
+    #     return (x, p - y)
+
+    # Derive the y-component of a compressed point, given the x-component only, assuming the y-component is odd.
+    def uncompress_point_odd(x, p, a, b):
+        y = nummaster.basic.sqrtmod(pow(x, 3, p) + a * x + b, p)
+        return y
+
+
+    # "4.1.6 Public Key Recovery Operation" from http://www.secg.org/sec1-v2.pdf -
+
+    (r,s) = extract_r_s_from_signature(signature)
+
+    candidate_public_key_points = []
+
+    for j in [0, 1]:  # "h" of the curve is 1.
+
+        x = r + j * n
+        print(f"x = {x}")
+        try:
+            y = uncompress_point_odd(x, p, a, b)
+            print(f"y = {y}")
+        except Exception:
+            print("Decompressing point failed, trying next.")
+            continue
+
+        R = tinyec.ec.Point(curve, x, y)
+        print(f"R = {R}")
+
+        n_dot_R = n * R
+        print(f"n*R = {n_dot_R}")
+        if n_dot_R != tinyec.ec.Inf(curve):
+            print("n*R isn't Inf, trying next.")
+            continue
+
+        digest = Crypto.Hash.SHA256.new(message)
+        e = int(digest.hexdigest(), 16)
+        for k in [1, -1]:  # Try both with a positive and a negative R.
+            Q = pow(r, -1, n) * (s * (k * R) - e * G)
+            print(f"Q = {Q}")
+
+            candidate_public_key_points.append(Q)
+
+    valid_public_keys = []
+    for candidate_public_key_point in candidate_public_key_points:
+        Crypto_candidate_public_key_point = Crypto.PublicKey.ECC.EccPoint(candidate_public_key_point.x,
+                                                                          candidate_public_key_point.y, curve=CURVE_NAME)
+        Crypto_candidate_public_key = Crypto.PublicKey.ECC.EccKey(point=Crypto_candidate_public_key_point, curve=CURVE_NAME)
+
+        verifier = Crypto.Signature.DSS.new(Crypto_candidate_public_key, CRYPTO_DOME_RANDOM_SIGNATURE_MODE)
+        try:
+            verifier.verify(digest, signature)
+            # Signature matches, store the public key.
+            valid_public_keys.append(Crypto_candidate_public_key)
+        except ValueError:
+            # Signature doesn't match, continue.
+            continue
+
+    return valid_public_keys
+
+
+def main():
+
+    message = b'Message'
+
+    keypair = generate_keypair()
+    signature = generate_signature(keypair, message)
+
+
+    print ("=== Signature Verification Using PyCryptodome ===")
+
+    valid = verify_signature_cryptodome(keypair.public_key(), message, signature)
+    if valid:
+        print("----> Signature matches.")
+    else:
+        print("----> Signature DOESN'T match!")
+        sys.exit(1)
+
+
+    print ("=== Signature Verification Using ECC Calculations ===")
+
+    valid = verify_signature_ecc_calcs(keypair.public_key(), message, signature)
+    if valid:
+        print("----> Signature matches.")
+    else:
+        print("----> Signature DOESN'T match!")
+        sys.exit(1)
+
+
+    print ("=== Public Key from Signature Using ECC Calculations ===")
+
+    public_keys = public_keys_from_signature(message, signature)
+    print("----> Valid public keys that can verify the signature -")
+    for public_key in public_keys:
+        print(public_key)
+        if public_key == keypair.public_key():
+            print ("(^^^ is identical to the original public key)")
+
+main()
